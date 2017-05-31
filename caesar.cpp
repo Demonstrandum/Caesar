457344f42bab6058ccc9a2df943e9280ecf235c9
Demonstrandum <knutsen@jetspace.co>
2017-05-31 19:30:47 +0100

    Added bruteforce option instead of stating rotation amount

--- caesar.cpp
+++ caesar.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
 bool IsInString(string str, char element) {
-  for(unsigned int i = 0; i < str.length(); i++){
+  for(unsigned int i = 0; i < str.length(); i++) {
      if(str[i] == element){
          return true;
      }
@@ -27,7 +29,7 @@ string rot(int n, string str) {
   string upperAlph = alph;
   string upperCiph = ciph;
   transform(upperAlph.begin(), upperAlph.end(), upperAlph.begin(), ::toupper);
-  transform(upperCiph.begin(), upperCiph.end(), upperCiph.begin(), ::toupper);
+  transform(upperCiph.begin(), upperCiph.end(), upperCiph.begin(), ::toupper); // or alph.upcase in Ruby or alph.upper() in Python...
 
   unsigned int i = 0;
   unsigned int j = 0;
@@ -61,8 +63,7 @@ int main(int argc, char** argv) {
     return 1;
   }
   string content;
-  string strRotation = argv[1];
-  int rotation = atoi(strRotation.c_str());
+
   if (string(argv[2]) == "text") {
     content = string(argv[3]);
   }
@@ -76,6 +77,18 @@ int main(int argc, char** argv) {
     cout << "Please choose a way of retrieving the input (text or file)." << endl;
     return 1;
   }
+
+  string strRotation = argv[1];
+  if (strRotation == "bruteforce" || strRotation == "brute" || strRotation == "force") {
+    for (unsigned int i = 25; i >= 1; i--) {
+      stringstream list;
+      list << setfill('0') << setw(2) << -(i - 26); // Adds padding with '0's, e.g. {5 => 05, 14 => 14} etc...
+      cout << list.str() << " => " << rot(i, content) << endl; // ^^^ (i - 26) * -1 to reverse the numbers because brute force decrypts, showing the encryption number as opposed to the decryption number
+    }
+    return 0;
+  }
+
+  int rotation = atoi(strRotation.c_str());
   cout << rot(rotation, content);
   return 0;
 }
